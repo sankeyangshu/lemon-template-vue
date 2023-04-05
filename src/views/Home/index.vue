71bcf27091cc1c40380255c8a25bb501ffbbd92c
sankeyangshu <1260323835@qq.com>
2023-04-05 14:47:02 +0800

    feat: 新增欢迎组件

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -3,12 +3,35 @@
  * @Author: 三棵杨树
  * @Date: 2023-04-03 20:19:06
  * @LastEditors: 三棵杨树
- * @LastEditTime: 2023-04-03 20:20:07
+ * @LastEditTime: 2023-04-05 14:44:52
 -->
 <template>
-  <div>hello world</div>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="../../assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import HelloWorld from '@/components/HelloWorld.vue';
+</script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
